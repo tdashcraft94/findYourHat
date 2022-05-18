42ff434de57eb4393688dcad837c6efcb2751db2
Taylor <tdashcraft94@gmail.com>
2022-05-18 10:19:45 -0600

    Add ideas for improvement to main

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 const play = require('play');
 
 
-
+//Ideas for extension, further development.
+// 1. Random start position.
+// 2. "Hard Mode" where holes are added as you play
+// 3. Colors and other packages to help make the game more graphically interesting and interactive
+// 4. Path Validator, to ensure that pathway can be solved (psuedo maze solver)
 
 
 
